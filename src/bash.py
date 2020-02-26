619c58f9a7447047dbad19f7d6248564da642ac3
David Alves <david2000tsi@gmail.com>
2020-02-26 16:18:53 -0300

    Added option to encrypt and decrypt messages

--- src/bash.py
+++ src/bash.py
@@ -1,6 +1,21 @@
 import sys
 import os
 from sys import platform
+from cryptography.fernet import Fernet
+
+class Cryptography:
+
+	key = Fernet.generate_key()
+
+	def encrypt(txt):
+		cipher = Fernet(Cryptography.key)
+		txt_byte = str(txt).encode()
+		return (cipher.encrypt(txt_byte)).decode()
+
+	def decrypt(chp_txt):
+		cipher = Fernet(Cryptography.key)
+		txt_byte = str(chp_txt).encode()
+		return (cipher.decrypt(txt_byte)).decode()
 
 class Bash:
 
@@ -12,6 +27,8 @@ class Bash:
 		print('Usage:')
 		print('> ? - to show help')
 		print('> os - to show native os')
+		print('> enc <msg> - to encrypt msg')
+		print('> dec <msg> - to decrypt msg')
 		print('> \\c - to clear screen')
 		print('> \\q - to quit bash')
 		Bash.new_line();
@@ -35,23 +52,44 @@ class Bash:
 		while True:
 			choise = input()
 			custom_new_line = False
+			valid = False
 
 			if(choise == '?'):
 				Bash.help();
+				valid = True
 
 			if(choise == 'os'):
 				Bash.print_platform();
 				custom_new_line = True
+				valid = True
+
+			if(choise.find('enc ') == 0):
+				txt = choise.replace('enc ', '')
+				print(Cryptography.encrypt(txt))
+				custom_new_line = True
+				valid = True
+
+			if(choise.find('dec ') == 0):
+				txt = choise.replace('dec ', '')
+				print(Cryptography.decrypt(txt))
+				custom_new_line = True
+				valid = True
 
 			if(choise == '\\c'):
 				os.system(Bash.cmd_clear())
 				custom_new_line = True
+				valid = True
 
 			if(choise == ''):
 				custom_new_line = True
+				valid = True
 
 			if(choise == '\\q'):
 				break
+
+			if(valid == False):
+				print('Invalid option!')
+				custom_new_line = True
 
 			if(custom_new_line):
 				Bash.new_line();
